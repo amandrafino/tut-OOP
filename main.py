b09a6bc3754d12e98c97ffca84220f0881d37956
amandrafino <amandrafino@gmail.com>
2024-03-25 17:37:05 -0400

    first commit

---
+++ main.py
@@ -0,0 +1,24 @@
+#!/usr/local/bin/python3
+
+# Class definition for an item with name, price, and quantity attributes
+class Item:
+    def  __init__(self, name, price, quantity):
+        # Comment out or remove the print statement for production
+        # print(f"An instance has been created: {name}")
+        self.name = name
+        self.price = price
+        self.quantity = quantity
+    
+    # Method to calculate total price using the item's price and quantity
+    def calculate_total_price(self):
+        return self.price * self.quantity
+
+# Instantiate two items
+item1 = Item("iPhoneSE", 100, 5)
+item2 = Item("iPhone 14", 1000, 3)
+
+# Demonstrating the use of attributes and methods
+print(item1.name, item1.calculate_total_price())
+print(item2.name, item2.calculate_total_price())
+
+
