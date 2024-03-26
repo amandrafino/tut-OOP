109049a45a553be20f3673ff42a8e77f0393a469
amandrafino <amandrafino@gmail.com>
2024-03-26 09:56:32 -0400

    basic first commit

--- main.py
+++ main.py
@@ -2,9 +2,12 @@
 
 # Class definition for an item with name, price, and quantity attributes
 class Item:
-    def  __init__(self, name, price, quantity=0):
-        # Comment out or remove the print statement for production
-        # print(f"An instance has been created: {name}")
+    def  __init__(self, name: str, price: float, quantity: int):
+        # Run validations to the received arguments
+        assert price >= 0, f"Price ({price}) must be greater than zero."
+        assert quantity >= 0
+
+        # Assign to self object
         self.name = name
         self.price = price
         self.quantity = quantity
@@ -22,4 +25,13 @@ item2 = Item("iPhone 14", 1000, 3)
 print(item1.name, item1.calculate_total_price())
 print(item2.name, item2.calculate_total_price())
 
+class Memory(Item):
+    def __init__(self, name, price, quantity, memory):
+        super().__init__(name, price, quantity)
+        self.memory= memory
+
+
+item2 = Memory("iPhone 14", 1000, 3, True )
+print(item2.memory)
+print (f"This iPhone: ({item2.name}) comes with 2 gigs of memory")
 
