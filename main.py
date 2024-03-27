df14d589384af5cee1bbd4ba36303349860cd8ac
amandrafino <amandrafino@gmail.com>
2024-03-27 13:32:03 -0400

    first commit

--- main.py
+++ main.py
@@ -1,37 +1,37 @@
-#!/usr/local/bin/python3
+class Item(object):
+    # constructor
+    def __init__(self, name:str, price:float, quantity=0):
+        # Run validations
+        assert price >= 0, f"Price {price} must be greater than zero!"
+        assert quantity >= 0, f"Quantity {quantity} must be greate than zero!"
 
-# Class definition for an item with name, price, and quantity attributes
-class Item:
-    def  __init__(self, name: str, price: float, quantity: int):
-        # Run validations to the received arguments
-        assert price >= 0, f"Price ({price}) must be greater than zero."
-        assert quantity >= 0
-
-        # Assign to self object
+        # assign to self object
         self.name = name
         self.price = price
         self.quantity = quantity
-    
-    # Method to calculate total price using the item's price and quantity
+
     def calculate_total_price(self):
         return self.price * self.quantity
 
-# Instantiate two items
-item1 = Item("iPhoneSE", 100, 5)
-item2 = Item("iPhone 14", 1000, 3)
+item1 = Item("phone", 100, 5)
+print(f"{item1.name}: ${item1.price}")
+print(item1.calculate_total_price())
+
+print("\n") # line break
+
+item2 = Item("laptop", 1000, 2)
+print(f"{item2.name}: ${item2.price}")
+print(item2.calculate_total_price())
+
+item2.has_numpad = False
 
 
-# Demonstrating the use of attributes and methods
-print(item1.name, item1.calculate_total_price())
-print(item2.name, item2.calculate_total_price())
-
-class Memory(Item):
-    def __init__(self, name, price, quantity, memory):
-        super().__init__(name, price, quantity)
-        self.memory= memory
-
-
-item2 = Memory("iPhone 14", 1000, 3, True )
-print(item2.memory)
-print (f"This iPhone: ({item2.name}) comes with 2 gigs of memory")
-
+"""# {{{
+When you invoke a method on an instance in Python, the Py runtime
+automatically passes the instance itself as the first argument
+to the method. This argument is named (self) in the method's
+parameter list.
+This allows the methods to access and manipulate the instance's
+attributes and other methods, facilitating Object Oriented 
+behaviors and interactions within the class.
+"""# }}}
